4f6f370c09ad552b662208d609a308d4ed08e791
Leandro Toloza <leandrotoloza8@gmail.com>
2021-02-14 23:56:10 -0300

    rotation and verify in grid if there are figures that interrupt fall/movement

--- Console-Tetris-.Net/Console-Tetris-.Net/TetrisFigure.cs
+++ Console-Tetris-.Net/Console-Tetris-.Net/TetrisFigure.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Console_Tetris_.Net
 {
@@ -14,24 +15,24 @@ namespace Console_Tetris_.Net
         public static int[,] Figure6 = new int[2, 3] { { 1, 0, 0 }, { 1, 1, 1 } }; // ""  _|  "" J
         public static int[,] Figure7 = new int[2, 3] { { 0, 0, 1 }, { 1, 1, 1 } }; // ""  |_  "" L
 
-        public static bool isDropped = false;
-
         //List with all the figures
         public static List<int[,]> tetrisFigures = new List<int[,]>() { Figure1, Figure2, Figure3, Figure4, Figure5, Figure6, Figure7 };
 
+        //Display
         private int[,] block;
         public List<int[]> location = new List<int[]>();
+        private bool isErect = false;
+  
 
+        //ctor of the Tetris Figure
         public TetrisFigure()
         {
             Random rnd = new Random();
             block = tetrisFigures[rnd.Next(0, 7)];
             Console.Clear();
             Program.DrawBorder();
-            Program.GetDashboard();
+            Program.GetDashboard(1,0,0);
             ShowTetrisFigureOnDashboard();
-            
-
             
         }
 
@@ -67,7 +68,7 @@ namespace Console_Tetris_.Net
             Update();
         }
 
-
+        //draw the figure when we actualize the console
         public static void Draw()
         {
             for (int i = 0; i < 23; ++i)
@@ -88,6 +89,8 @@ namespace Console_Tetris_.Net
 
             }
         }
+
+        //update the console
         public void Update()
         {
             for (int i = 0; i < 23; i++)
@@ -104,27 +107,321 @@ namespace Console_Tetris_.Net
             Draw();
         }
 
-        //This is the loop that updates the console every time when the figure is falling down
-        public static void ActivityLoop()
+        //Funcion that Fall down the tetris figure
+        public void Drop()
         {
-            while (true)
+
+            if (isSomethingBelow())
             {
-
-                if (TetrisFigure.isDropped == true)
+                for (int i = 0; i < 4; i++)
                 {
-                    tFig = nextTFig;
-                    nextTFig = new TetrisFigure();
-                    tFig.DisplayFigure();
-
-                    TetrisFigure.isDropped = false;
+                    Program.spawnedBlockLocation[location[i][0], location[i][1]] = 1; //should change name to dropped block location
                 }
-                for (int j = 0; j < 10; j++)
-                {
-                    if (spawnedBlockLocation[0, j] == 1)
-                        return;
-                }
+                Program.isDropped = true;
 
             }
+            else
+            {
+                for (int numCount = 0; numCount < 4; numCount++)
+                {
+                    location[numCount][0] += 1;
+                }
+                Update();
+            }
+        }
+
+
+        //IN THIS PART THERE ARE METHODS OF MOVEMENT
+
+        //Rotate function analizes all the figures and what to verify if we want to rotate it
+        public void Rotate()
+        {
+            List<int[]> templocation = new List<int[]>();
+            for (int i = 0; i < block.GetLength(0); i++)
+            {
+                for (int j = 0; j < block.GetLength(1); j++)
+                {
+                    if (block[i, j] == 1)
+                    {
+                        templocation.Add(new int[] { i, (10 - block.GetLength(1)) / 2 + j });
+                    }
+                }
+            }
+
+            if (block == tetrisFigures[0])
+            {
+                if (isErect == false)
+                {
+                    for (int i = 0; i < location.Count; i++)
+                    {
+                        templocation[i] = TransformMatrix(location[i], location[2], "Clockwise");
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < location.Count; i++)
+                    {
+                        templocation[i] = TransformMatrix(location[i], location[2], "Counterclockwise");
+                    }
+                }
+            }
+
+            else if (block == tetrisFigures[3])
+            {
+                for (int i = 0; i < location.Count; i++)
+                {
+                    templocation[i] = TransformMatrix(location[i], location[3], "Clockwise");
+                }
+            }
+
+            else if (block == tetrisFigures[1]) return;
+            else
+            {
+                for (int i = 0; i < location.Count; i++)
+                {
+                    templocation[i] = TransformMatrix(location[i], location[2], "Clockwise");
+                }
+            }
+
+
+            for (int count = 0; isOverlayLeft(templocation) != false | isOverlayRight(templocation) != false | isOverlayBelow(templocation) != false; count++)
+            {
+                if (isOverlayLeft(templocation) == true)
+                {
+                    for (int i = 0; i < location.Count; i++)
+                    {
+                        templocation[i][1] += 1;
+                    }
+                }
+
+                if (isOverlayRight(templocation) == true)
+                {
+                    for (int i = 0; i < location.Count; i++)
+                    {
+                        templocation[i][1] -= 1;
+                    }
+                }
+                if (isOverlayBelow(templocation) == true)
+                {
+                    for (int i = 0; i < location.Count; i++)
+                    {
+                        templocation[i][0] -= 1;
+                    }
+                }
+                if (count == 3)
+                {
+                    return;
+                }
+            }
+
+            location = templocation;
+
+        }
+
+        // we analize the figure when we want to transform it and it depends of the clock wise direction
+        public int[] TransformMatrix(int[] coord, int[] axis, string dir)
+        {
+            int[] pcoord = { coord[0] - axis[0], coord[1] - axis[1] };
+            if (dir == "Counterclockwise")
+            {
+                pcoord = new int[] { -pcoord[1], pcoord[0] };
+            }
+            else if (dir == "Clockwise")
+            {
+                pcoord = new int[] { pcoord[1], -pcoord[0] };
+            }
+
+            return new int[] { pcoord[0] + axis[0], pcoord[1] + axis[1] };
+        }
+
+
+        //check if there is something below, and the same for the other ones IS SOMETHING
+        public bool isSomethingBelow()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (location[i][0] + 1 >= 23)
+                    return true;
+                if (location[i][0] + 1 < 23)
+                {
+                    if (Program.spawnedBlockLocation[location[i][0] + 1, location[i][1]] == 1)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool? isOverlayBelow(List<int[]> location)
+        {
+            List<int> ycoords = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                ycoords.Add(location[i][0]);
+                if (location[i][0] >= 23)
+                    return true;
+                if (location[i][0] < 0)
+                    return null;
+                if (location[i][1] < 0)
+                {
+                    return null;
+                }
+                if (location[i][1] > 9)
+                {
+                    return null;
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (ycoords.Max() - ycoords.Min() == 3)
+                {
+                    if (ycoords.Max() == location[i][0] | ycoords.Max() - 1 == location[i][0])
+                    {
+                        if (Program.spawnedBlockLocation[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (ycoords.Max() == location[i][0])
+                    {
+                        if (Program.spawnedBlockLocation[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+
+        public bool isSomethingLeft()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (location[i][1] == 0)
+                {
+                    return true;
+                }
+                else if (Program.spawnedBlockLocation[location[i][0], location[i][1] - 1] == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool? isOverlayLeft(List<int[]> location)
+        {
+            List<int> xcoords = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                xcoords.Add(location[i][1]);
+                if (location[i][1] < 0)
+                {
+                    return true;
+                }
+                if (location[i][1] > 9)
+                {
+                    return false;
+                }
+                if (location[i][0] >= 23)
+                    return null;
+                if (location[i][0] < 0)
+                    return null;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (xcoords.Max() - xcoords.Min() == 3)
+                {
+                    if (xcoords.Min() == location[i][1] | xcoords.Min() + 1 == location[i][1])
+                    {
+                        if (Program.spawnedBlockLocation[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (xcoords.Min() == location[i][1])
+                    {
+                        if (Program.spawnedBlockLocation[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool isSomethingRight()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (location[i][1] == 9)
+                {
+                    return true;
+                }
+                else if (Program.spawnedBlockLocation[location[i][0], location[i][1] + 1] == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool? isOverlayRight(List<int[]> location)
+        {
+            List<int> xcoords = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                xcoords.Add(location[i][1]);
+                if (location[i][1] > 9)
+                {
+                    return true;
+                }
+                if (location[i][1] < 0)
+                {
+                    return false;
+                }
+                if (location[i][0] >= 23)
+                    return null;
+                if (location[i][0] < 0)
+                    return null;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (xcoords.Max() - xcoords.Min() == 3)
+                {
+                    if (xcoords.Max() == location[i][1] | xcoords.Max() - 1 == location[i][1])
+                    {
+                        if (Program.spawnedBlockLocation[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (xcoords.Max() == location[i][1])
+                    {
+                        if (Program.spawnedBlockLocation[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
         }
 
     }
